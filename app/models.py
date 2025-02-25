25e4425a98b27127cda9222406f3a84d960908c4
Cristian <cristiangaming5505@gmail.com>
2025-02-25 05:04:07 -0500

    added classes to models

--- app/models.py
+++ app/models.py
@@ -13,4 +13,63 @@ class User(db.Model, UserMixin):
     username = db.Column(db.String(32), index=True, unique=True, nullable=False)
     password = db.Column(db.String(64), index=True, nullable=False)
     email = db.Column(db.String(64), index=True, unique=True, nullable=False)
-    admin = db.Column(db.Boolean, index=True, default=False)
+    admin = db.Column(db.Boolean, index=True, default=False) #site admin, different from group admin / mod
+
+    study_groups = db.relationship('StudyGroup', backref='owner', lazy=True) #one to many, user can create multiple study groups
+    memberships = db.relationship('Member', backref='user', lazy=True)#one to many, user can be members of multiple groups
+    messages = db.relationship('Message', backref='author', lazy=True)#one to many, user can send multiple messages
+    
+    def set_password(self, password):
+        self.password = generate_password_hash(password) 
+
+    def check_password(self, password):
+        return check_password_hash(self.password, password)
+
+    def __repr__(self):
+        return '{} {}'.format(self.username, self.email)
+    
+class StudyGroup(db.Model):
+    __tablename__='studygroup'
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(64), index=True, unique=True, nullable=False)
+    description = db.Column(db.Text, index=True, nullable=False)
+    owner_id = db.Column(db.Integer, db.ForeignKey('user.id'), index=True, nullable=False)
+
+    members = db.relationship('Member', backref='group', lazy=True) #one to many, study group can have many members
+    messages = db.relationship('Message', backref='group', lazy=True) #one to many, study group can have many messages
+
+class Member(db.Model): #many to many, many users can be members of many groups
+    __tablename__ = 'member'
+    id = db.Column(db.Integer, primary_key=True)
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'), index=True, nullable=False)
+    group_id = db.Column(db.Integer, db.ForeignKey('studygroup.id'), index=True, nullable=False)
+    moderator = db.Column(db.Boolean, default=False)
+
+class Announcement(db.Model):
+    __tablename__ = 'announcement'
+    id = db.Column(db.Integer, primary_key=True)
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'), index=True, nullable=False)
+    group_id = db.Column(db.Integer, db.ForeignKey('studygroup.id'), index=True, nullable=False)
+    text = db.Column(db.Text, nullable=False)
+    
+    user = db.relationship('User', backref='announcements') #one to many, user can make many announcements
+    group = db.relationship('StudyGroup', backref='announcements') #one to many, group can have many announcements
+
+class Note(db.Model):
+    __tablename__ = 'note'
+    id = db.Column(db.Integer, primary_key=True)
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'), index=True, nullable=False)
+    group_id = db.Column(db.Integer, db.ForeignKey('studygroup.id'), index=True, nullable=False)
+    file_path = db.Column(db.String(256), nullable=False)
+    description = db.Column(db.Text, nullable=True)
+
+    user = db.relationship('User', backref='notes') #one to many, user can have multiple notes
+    group = db.relationship('StudyGroup', backref='notes') #one to many, group can have multiple notes
+
+class Message(db.Model):
+    __tablename__ = 'message'
+    id = db.Column(db.Integer, primary_key=True)
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'), index=True, nullable=False)
+    group_id = db.Column(db.Integer, db.ForeignKey('studygroup.id'), index=True, nullable=False)
+    message = db.Column(db.Text, nullable=False)
+    time = db.Column(db.DateTime, default=datetime.now)
